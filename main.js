558f9511a0ec9e6595b66e686ed93f1a52a52cb8
Zach Chaney <zachary.chaney@gmail.com>
2015-05-20 22:29:20 -0500

    final commit

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //lets display the current time
 var d, h, m, s, clock;
 function displayTime() {
-	d = new Date(); //new data object
+	d = new Date();
 	h = d.getHours();
 	m = d.getMinutes();
 	s = d.getSeconds();
 	
-	//add zero to the left of the numbers if they are single digits
+//add zero to the left of the numbers if they are single digits
 if(h <= 9) {
 	h = '0'+h;
 }
@@ -20,19 +20,15 @@ if(s <= 9) {
 }
 
 clock = h+":"+m+":"+s;
-//set time
+
 document.getElementById("clkcolor").innerHTML = clock;
 	
-var colors = ["#0099cc","#c0c0c0","#587b2e","#990000","#1C8200","#987baa","#464646","#AA8971","#1987FC","#99081E"];
+var colors = ["#221752","#c0c0c0","#587b2e","#000000","#464646","#99081E"];
 
-setInterval(function() {
-
-var bodybgarrayno = Math.floor(Math.random() * colors.length);
-var selectedcolor = colors[bodybgarrayno];
+var bodyColor = Math.floor(Math.random() * colors.length);
+var selectedcolor = colors[bodyColor];
 
 document.body.style.background = selectedcolor;
-	}, 2000);
 }
 
-//call the function
 setInterval(displayTime, 1000);
