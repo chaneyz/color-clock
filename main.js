547479ab76b6f36ba97cb2227466701763b73259
Zach Chaney <zachary.chaney@gmail.com>
2015-05-20 21:34:32 -0500

    final sub done

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //lets display the current time
-var d, h, m, s, color;
+var d, h, m, s, clock;
 function displayTime() {
 	d = new Date(); //new data object
 	h = d.getHours();
@@ -7,40 +7,32 @@ function displayTime() {
 	s = d.getSeconds();
 	
 	//add zero to the left of the numbers if they are single digits
-	if(h <= 9) h = '0'+h;
-	if(m <= 9) m = '0'+m;
-	if(s <= 9) s = '0'+s;
+if(h <= 9) {
+	h = '0'+h;
+}
+
+if(m <= 9) {
+	m = '0'+m;
+}
+
+if(s <= 9) {
+	s = '0'+s;
+}
+
+clock = h+":"+m+":"+s;
+//set time
+document.getElementById("clkcolor").innerHTML = clock;
 	
-	color = "#"+h+m+s;
-	//set background color
-	document.body.style.background = color;
-	//set time
-	document.getElementById("hex").innerHTML = color;
-	
-	//retrigger the function every second
-	setTimeout(displayTime, 1000);
+var colors = ["#0099cc","#c0c0c0","#587b2e","#990000","#1C8200","#987baa","#464646","#AA8971","#1987FC","#99081E"];
+
+setInterval(function() {
+
+var bodybgarrayno = Math.floor(Math.random() * colors.length);
+var selectedcolor = colors[bodybgarrayno];
+
+document.body.style.background = selectedcolor;
+	}, 2000);
 }
 
 //call the function
-displayTime();
-
-
-// function updateClock() {
-	
-// }
-
-
-// function changeBackground() {
-
-// var colors = ["#0099cc","#c0c0c0","#587b2e","#990000","#1C8200","#987baa","#464646","#AA8971","#1987FC","#99081E"];
-
-// setInterval(function() {
-
-// var bodybgarrayno = Math.floor(Math.random() * colors.length);
-// var selectedcolor = colors[bodybgarrayno];
-
-// document.body.style.background = selectedcolor;
-// 	}, 1000);
-// }
-
-// changeBackground();
+setInterval(displayTime, 1000);
